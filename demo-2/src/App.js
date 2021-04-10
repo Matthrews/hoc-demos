220b37e802a3a6cc9d4bab1bdb193090d1eda761
Matthew <Matthrews@outlook.com>
2021-04-10 15:19:07 +0800

    高阶组件-反向继承

--- demo-2/src/App.js
+++ demo-2/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Learn React</h1>
+                <p>
+                    {JSON.stringify(this.state, null, 2)}
+                    {JSON.stringify(this.props, null, 2)}
+                </p>
+            </div>
+        );
+    }
 }
 
-export default App;
+
+function withLogging(WrappedComponent) {
+    class Enhance extends WrappedComponent {
+        UNSAFE_componentWillReceiveProps(nextProps) {
+            console.log('Current props', this.props);
+            console.log('Next props', nextProps);
+        }
+
+        render() {
+            const {forwardedRef, ...rest} = this.props;
+            // 把 forwardedRef 赋值给 WrappedComponent 的 ref
+            return <WrappedComponent {...rest} ref={forwardedRef}/>;
+        }
+    }
+
+    // React.forwardRef 方法会传入 props 和 ref 两个参数给其回调函数
+    // 所以这边的 ref 是由 React.forwardRef 提供的
+    return React.forwardRef((props, ref) => {
+        return <Enhance {...props} data={123} forwardRef={ref}/>
+    });
+}
+
+
+export default withLogging(App)
